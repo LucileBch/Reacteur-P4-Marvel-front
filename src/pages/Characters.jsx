58168f798c82aeab0e3a94afecfb80c353f5081a
LucileBch <l.bouchire@gmail.com>
2024-04-03 19:41:15 +0200

    Characters page : displaying all characters

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,6 +1,52 @@
 // ---------- CHARACTERS Page ----------
+// Packages Imports
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// Components Imports
+import Card from "../components/Card";
+
 const Characters = () => {
-  return <p>Character Page</p>;
+  // Fetch API datas with useEffect
+  // Check server response
+  //    If waiting for datas : display "loading"
+  //    Else : display content
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(`http://localhost:3000/characters`);
+      setData(data);
+      setIsLoading(false);
+
+      console.log("ICI LA DATA", data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // Array of characters
+  const charactersArray = data.results;
+
+  return (
+    <>
+      {isLoading === true ? (
+        "Loading"
+      ) : (
+        <main>
+          <h1>Les Personnages de Marvel</h1>
+          <section style={{ display: "flex", flexWrap: "wrap" }}>
+            <Card infosArray={charactersArray} />
+          </section>
+        </main>
+      )}
+    </>
+  );
 };
 
 // Export page
